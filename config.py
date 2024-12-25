a08c46f7a7a440cab3f072f6ca994cf262509e5c
Hemanth-9087 <130548262+Hemanth-9087@users.noreply.github.com>
2024-12-25 21:07:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ API_KEYS = {
     "weather": "3d292cd40cedd75da4fdf04ea8479338",
 }
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///favorites.db"  # Replace with your preferred database URI
+SQLALCHEMY_DATABASE_URI = "sqlite:///favorites.db"  
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SECRET_KEY = "QWERTYLKJ890"
