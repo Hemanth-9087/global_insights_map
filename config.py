2bb7209c3cdccb6a8fa9788eaa63c16b2961d9b9
Hemanth-9087 <130548262+Hemanth-9087@users.noreply.github.com>
2025-12-30 13:25:15 +0530

    Merge remote and local

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 # config.py
 API_KEYS = {
     "weather": "3d292cd40cedd75da4fdf04ea8479338",
+    "pollution": "ee197d661d30e78245bbe7483a81912a8314c56b95ed8dabd8e60dcb1b9e96a3",
 }
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///favorites.db"  
+SQLALCHEMY_DATABASE_URI = "sqlite:///favorites.db"  # Replace with your preferred database URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SECRET_KEY = "QWERTYLKJ890"
